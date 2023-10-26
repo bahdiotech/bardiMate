a0f48e718d70ebfdcc50ea0a1428ec5b6201227f
ab.bard139@gmail.com <ab.bard139@gmail.com>
2023-10-26 01:31:50 -0500

    I made some changes

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,7 +1,6 @@
 
 import { Routes, Route} from "react-router-dom";
 import { MovieList, MovieDetail, Search, PageNotFound} from "../pages";
-import {  } from "../pages/ReviewCard";
 
 export const AllRoutes = () => {
   
