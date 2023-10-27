d7d10b7962c2556abc29c26902d492d147c4a55e
ab.bard139@gmail.com <ab.bard139@gmail.com>
2023-10-26 21:16:07 -0500

    I made changes on the card and made sure I formatted my money objects to huminize form

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,13 @@
-import React from "react";
+
 import { Link } from "react-router-dom";
 import backup from '../assets/images/backup.png'
+
 
 export const Card = ({movie}) => {
   const {id, title, overview, poster_path } = movie
   const image = poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : backup;
+  
+
   return (
     <div>
       <div className="max-w-sm h-[50rem] overflow-hidden bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-4">
@@ -18,13 +21,17 @@ export const Card = ({movie}) => {
         </Link>
         <div className="p-5">
           <Link to={`/movie/${id}`}>
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-justify text-gray-900 dark:text-white">
             {title}
             </h5>
           </Link>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          { overview.length <= 240 ? 
+          (<p className="mb-3 font-normal text-gray-700 text-justify dark:text-gray-400">
             {overview}
-          </p>
+          </p>) :  (<p className="mb-3 font-normal text-gray-700 text-justify dark:text-gray-400">
+            {overview.slice(0,240)}.. <Link to={`/movie/${id}`} className="text-blue-400 cursor-pointer">read more..</Link>
+          </p> )
+}
         </div>
       </div>
     </div>
